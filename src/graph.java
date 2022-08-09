9c95e6ea944e50a65c90ba8f850ff3dcac276328
kbrsw <kbroz82@gmail.com>
2022-08-09 19:57:44 +0300

    remake graph. Added method "fun"

--- src/graph.java
+++ src/graph.java
@@ -13,29 +13,31 @@ import java.io.File;
 import java.io.IOException;
 
 
-    public class graph {
+public class graph {
 
+    public static double fun(double x) {
 
-        public static void cycle() throws IOException {
-
-
-            XYDataset dataset1 = DatasetUtilities.sampleFunction2D(x -> 2*Math.sin(x), -10, 10, 1000, "Function");
-
-            final JFreeChart chart = ChartFactory.createScatterPlot("Function", "x", "sin(x)", dataset1, PlotOrientation.VERTICAL, true, true, false);
-
-            ChartPanel chartPanel = new ChartPanel(chart);
-            chartPanel.setPreferredSize(new Dimension(500, 270));
-
-            ApplicationFrame frame = new ApplicationFrame("Example");
-
-            frame.setContentPane(chartPanel);
-            frame.pack();
-            frame.setVisible(true);
-
-        }
-
-
-        public static void main(String[] args) throws IOException {
-            cycle();
-        }
+        return Math.sin(x);
     }
+
+    public static void cycle() throws IOException {
+        XYDataset dataset1 = DatasetUtilities.sampleFunction2D(x -> fun(x), -10, 10, 1000, "Function");
+
+        final JFreeChart chart = ChartFactory.createScatterPlot("Function", "x", "sin(x)", dataset1, PlotOrientation.VERTICAL, true, true, false);
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new Dimension(500, 270));
+
+        ApplicationFrame frame = new ApplicationFrame("Example");
+
+        frame.setContentPane(chartPanel);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        cycle();
+    }
+}
