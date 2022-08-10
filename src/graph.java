26d1165986a332ae11149ecd2df0cd25801ba3b2
KostyaPC <kbroz82@gmail.com>
2022-08-10 17:19:04 +0300

    comments added

--- src/graph.java
+++ src/graph.java
@@ -17,25 +17,31 @@ public class graph {
 
     public static double fun(double x) {
 
-        return Math.sin(x);
+        return Math.sin(Math.log10(x));
     }
 
     public static void cycle() throws IOException {
-        XYDataset dataset1 = DatasetUtilities.sampleFunction2D(x -> fun(x), -10, 10, 1000, "Function");
+//      Compose dataset from our functions. Object dataset1 from class XYDataset
+        XYDataset dataset1 = DatasetUtilities.sampleFunction2D(x -> fun(x), 0, 10, 1000, "Function");
 
+//      Creating chart from class JFreeChart
         final JFreeChart chart = ChartFactory.createScatterPlot("Function", "x", "sin(x)", dataset1, PlotOrientation.VERTICAL, true, true, false);
 
+//      Create chartpanel and include it to chart
         ChartPanel chartPanel = new ChartPanel(chart);
+
+//      Customization of chartpanel
         chartPanel.setPreferredSize(new Dimension(500, 270));
 
+//      Add frame
         ApplicationFrame frame = new ApplicationFrame("Example");
 
         frame.setContentPane(chartPanel);
         frame.pack();
+//      Set frame visible
         frame.setVisible(true);
 
     }
-
 
     public static void main(String[] args) throws IOException {
         cycle();
